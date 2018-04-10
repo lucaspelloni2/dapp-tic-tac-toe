f07c28c20320a4eeab291573dc7443fd5cb29744
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-04-10 23:58:32 +0200

    get eth address from metamask extension

--- src/Game/test.js
+++ src/Game/test.js
@@ -2,37 +2,46 @@ import React, {Component} from 'react';
 import Web3 from 'web3';
 let web3 = window.web3;
 
+class EtherAddress extends Component {
+  constructor() {
+    super();
+    this.state = {ethAddress: ''};
+  }
 
+  componentDidMount() {
+    this.initWeb3();
+  }
 
-class Test extends Component {
+  getUserAddress() {
+    web3.eth
+      .getAccounts()
+      .then(addr => {
+        console.log(addr);
+        this.setState({ethAddress: addr.toString()});
+      })
+      .catch(err => {
+        console.log("error " + err);
+      });
+  }
 
-    constructor() {
-        super();
+  initWeb3() {
+    if (typeof web3 !== 'undefined') {
+      this.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+      console.log('existing web3: provider ' + web3.currentProvider);
+    } else {
+      this.web3Provider = new Web3.providers.HttpProvider(
+        'http://localhost:8545'
+      );
+      web3 = new Web3(this.web3Provider);
+      console.log('new web3');
     }
+    this.getUserAddress();
+  }
 
-    componentDidMount() {
-        this.initWeb3();
-    }
-
-    initWeb3() {
-        if (typeof web3 !== 'undefined') {
-            this.web3Provider = web3.currentProvider;
-            web3 = new Web3(web3.currentProvider);
-            console.log("existing web3: provider " + web3.currentProvider);
-        } else {
-
-            this.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
-            web3 = new Web3(this.web3Provider);
-            console.log("new web3");
-        }
-
-        // this.displayAccountInfo();
-        // this.initiateContract();
-    }
-
-    render() {
-        return (<div>Test component</div>);
-    }
+  render() {
+    return <div>Your Ethereum Address: {this.state.ethAddress}</div>;
+  }
 }
 
-export default Test;
+export default EtherAddress;
