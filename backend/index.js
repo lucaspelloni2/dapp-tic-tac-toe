78d5933a3e2f12cabd6fa90e7cc1391e4bf719ec
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-04-25 10:44:05 +0200

    asyncHandler in backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const app = express();
+const Web3 = require('web3');
 
+const asyncHandler = fn => {
+  return async function(request, response) {
+    try {
+      const data = await fn(request, response);
+      response.json({
+        success: true,
+        data
+      });
+    } catch (err) {
+      response.json({
+        success: false,
+        error: err.message
+      });
+      console.log(err);
+    }
+  };
+};
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-});
+app.get(
+  '/api',
+  asyncHandler(async (request, response) => {
+    console.log('api works');
+  })
+);
 
-app.listen(3000);
+app.get(
+  '/api/games',
+  asyncHandler(async (request, response) => {
+    console.log('api works');
+  })
+);
+
+app.listen(8080);
