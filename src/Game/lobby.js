c50332801f240ec244974653742a1ac70af9500e
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-04-25 15:19:49 +0200

    removed contract props

--- src/Game/lobby.js
+++ src/Game/lobby.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
 import ButtonLink from './Link';
-import ContractProps from './ContractProps';
 import MetaMaskLogo from "./MetamaskLogo";
 
 const ParentContainer = styled.div``;
