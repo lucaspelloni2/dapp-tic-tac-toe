852ded45c35e733023a1f2b64f206e25d57ce054
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-04-25 13:27:23 +0200

    link width props

--- src/Game/Link.js
+++ src/Game/Link.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 
-const ButtonLink = (props) => (
-    <Route
-        render={({history}) => (
-            <button
-                onClick={() => {
-                    history.push('/' + props.location.toString());
-                }}
-            >
-                {props.children}
-            </button>
-        )}
-    />
+const ButtonLink = props => (
+  <Route
+    render={({history}) => (
+      <button
+        style={{width: props.width}}
+        onClick={() => {
+          history.push('/' + props.location.toString());
+        }}
+      >
+        {props.children}
+      </button>
+    )}
+  />
 );
 
 export default ButtonLink;
-
