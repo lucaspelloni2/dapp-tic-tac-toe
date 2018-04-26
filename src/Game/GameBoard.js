2cbbfb121823fc10f1b38ac6e8dd7ad4b867182d
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-04-26 16:07:52 +0200

    GameBoard componenbt

---
+++ src/Game/GameBoard.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+
+class GameBoard extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <Container>Game board component</Container>;
+  }
+}
+
+export default GameBoard;
