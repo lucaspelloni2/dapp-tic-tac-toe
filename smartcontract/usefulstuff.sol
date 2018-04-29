e9c283da1d246ba95017557110ff6122f700bc7b
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-04-29 14:36:34 +0200

    added help file

---
+++ smartcontract/usefulstuff.sol
@@ -0,0 +1,24 @@
+    function stringToBytes32(string memory source) private pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+    
+        assembly {
+            result := mload(add(source, 32))
+        }
+    }
+    
+    /*
+    / motherfucking NOOOOT WOOORKING
+    */
+    function strConcat(string _a, string _b) internal returns (string) {
+        bytes memory _ba = bytes(_a);
+        bytes memory _bb = bytes(_b);
+        string memory ab = new string(_ba.length + _bb.length);
+        bytes memory bab = bytes(ab);
+        uint k = 0;
+        for (uint i = 0; i < _ba.length; i++) bab[k++] = _ba[i];
+        for (i = 0; i < _bb.length; i++) bab[k++] = _bb[i];
+        return string(bab);
+    }
