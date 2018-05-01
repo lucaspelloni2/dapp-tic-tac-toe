1f8f9695abf7e47587357c1ca8bf51be74c0f2fb
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-05-01 11:07:18 +0200

    brutal margin fixed in lobby

--- src/Game/lobby.js
+++ src/Game/lobby.js
@@ -35,7 +35,6 @@ const UserProfileContainer = styled.div`
   margin-top: 2em;
 `;
 
-
 const Button = styled.button`
   width: 250px;
 `;
@@ -85,54 +84,59 @@ class Lobby extends Component {
 
   render(props) {
     return (
-      <ParentContainer>
-        <UserProfileContainer>
-          <Profile>
-            <UserIcon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path d="M256 0c88.366 0 160 71.634 160 160s-71.634 160-160 160S96 248.366 96 160 167.634 0 256 0zm183.283 333.821l-71.313-17.828c-74.923 53.89-165.738 41.864-223.94 0l-71.313 17.828C29.981 344.505 0 382.903 0 426.955V464c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48v-37.045c0-44.052-29.981-82.45-72.717-93.134z" />
-            </UserIcon>
-            <UserName>{this.state.username}</UserName>
-          </Profile>
-        </UserProfileContainer>
-
-        <YourGames />
-        <Container>
-          <MetaMaskLogo />
-          <h1>Lobby</h1>
-          <p style={{fontSize: 22, marginTop: 0, marginBottom: 10}}>
-            Please select an option
-          </p>
-          <LobbyContainer>
-            <ButtonsContainer>
-              <ButtonContainer>
-                <ButtonLink
-                  width={250}
-                  location={'games/' + this.props.account.ethAddress}
-                >
-                  Create Game
-                </ButtonLink>
-              </ButtonContainer>
-              <ButtonContainer>
-                <ButtonLink width={250} location={'games'}>
-                  Join Game
-                </ButtonLink>
-              </ButtonContainer>
-              <ButtonContainer>
-                <Button>Make Bet</Button>
-              </ButtonContainer>
-              <ButtonContainer>
-                <Button>Join Bet</Button>
-              </ButtonContainer>
-              <ButtonContainer>
-                <Button>Rules</Button>
-              </ButtonContainer>
-              <ButtonContainer>
-                <Button>Log-Out</Button>
-              </ButtonContainer>
-            </ButtonsContainer>
-          </LobbyContainer>
-        </Container>
-      </ParentContainer>
+      <div>
+        <ParentContainer>
+          <UserProfileContainer>
+            <Profile>
+              <UserIcon
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path d="M256 0c88.366 0 160 71.634 160 160s-71.634 160-160 160S96 248.366 96 160 167.634 0 256 0zm183.283 333.821l-71.313-17.828c-74.923 53.89-165.738 41.864-223.94 0l-71.313 17.828C29.981 344.505 0 382.903 0 426.955V464c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48v-37.045c0-44.052-29.981-82.45-72.717-93.134z" />
+              </UserIcon>
+              <UserName>{this.state.username}</UserName>
+            </Profile>
+          </UserProfileContainer>
+          <div style={{marginTop: -122, marginBottom: 122}}>
+            <MetaMaskLogo />
+          </div>
+          <Container>
+            <h1>Lobby</h1>
+            <p style={{fontSize: 22, marginTop: 0, marginBottom: 10}}>
+              Please select an option
+            </p>
+            <LobbyContainer>
+              <ButtonsContainer>
+                <ButtonContainer>
+                  <ButtonLink
+                    width={250}
+                    location={'games/' + this.props.account.ethAddress}
+                  >
+                    Create Game
+                  </ButtonLink>
+                </ButtonContainer>
+                <ButtonContainer>
+                  <ButtonLink width={250} location={'games'}>
+                    Join Game
+                  </ButtonLink>
+                </ButtonContainer>
+                <ButtonContainer>
+                  <Button>Make Bet</Button>
+                </ButtonContainer>
+                <ButtonContainer>
+                  <Button>Join Bet</Button>
+                </ButtonContainer>
+                <ButtonContainer>
+                  <Button>Rules</Button>
+                </ButtonContainer>
+                <ButtonContainer>
+                  <Button>Log-Out</Button>
+                </ButtonContainer>
+              </ButtonsContainer>
+            </LobbyContainer>
+          </Container>
+        </ParentContainer>
+      </div>
     );
   }
 }
