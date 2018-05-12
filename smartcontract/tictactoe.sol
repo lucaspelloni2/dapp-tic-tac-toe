c08d41ed6874bb73d9c661c55dc34b14b825b4c9
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-05-12 17:49:12 +0200

    player cannot play against itself

--- smartcontract/tictactoe.sol
+++ smartcontract/tictactoe.sol
@@ -149,7 +149,7 @@ contract TicTacToe {
             emit Joined(true, game.gameId, game.state, playerName, "O");
         }
         else if (game.state == GameState.WAITING_FOR_X) {
-//            require(game.playerOAddr != msg.sender, "Player is already part of this game.");
+            require(game.playerOAddr != msg.sender, "Player is already part of this game.");
 
             game.playerXAddr = msg.sender;
             game.state = GameState.READY;
@@ -157,7 +157,7 @@ contract TicTacToe {
             emit Joined(true, game.gameId, game.state, playerName, "X");
         }
         else if (game.state == GameState.WAITING_FOR_O) {
-//            require(game.playerXAddr != msg.sender, "Player is already part of this game.");
+            require(game.playerXAddr != msg.sender, "Player is already part of this game.");
 
             game.playerOAddr = msg.sender;
             game.state = GameState.READY;
