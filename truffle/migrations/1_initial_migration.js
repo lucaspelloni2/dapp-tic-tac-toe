33dad9e4da0fff14a1f256486fe8d4941a6f3e32
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-05-15 19:03:29 +0200

    keep the SC here

--- truffle/migrations/1_initial_migration.js
+++ truffle/migrations/1_initial_migration.js
@@ -1,4 +1,4 @@
-var Migrations = artifacts.require('smartcontract/tictactoe.sol');
+var Migrations = artifacts.require('./tictactoe.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations).then(() => console.log(Migrations.address));
