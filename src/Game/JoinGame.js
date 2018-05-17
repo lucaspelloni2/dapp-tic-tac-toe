e242be1397341e2341c900c0f8753d487d6338e0
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-05-17 16:35:30 +0200

    changed joinGame method

--- src/Game/JoinGame.js
+++ src/Game/JoinGame.js
@@ -143,7 +143,7 @@ class JoinGame extends Component {
         this.props.setLoading(game, true);
       })
       .on('receipt', res => {
-        console.log();
+        this.props.setLoading(game, false);
         const returnValues = res.events.Joined.returnValues;
         let isSuccess =
           res.status.toString().includes('0x01') || res.status === '0x1'; // for private testnet || for metamask
@@ -155,7 +155,6 @@ class JoinGame extends Component {
               ' and has symbol ' +
               returnValues.symbol
           );
-          this.props.setLoading(game, false);
         } else {
           console.log('not possible to join');
         }
